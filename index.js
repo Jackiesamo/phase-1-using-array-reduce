9b0df0f0badf8457fba557e1a3dfaaf5487ed724
jacklyne <jacklyne.owuor@student.moringaschool>
2025-09-19 00:08:38 +0300

    added array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const BatteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteryBatches = [4, 5, 3, 4, 4, 5, 2, 0, 4];
+
+// Use reduce to sum all the numbers in batteryBatches
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+Module.exports = { totalBatteries };
+
